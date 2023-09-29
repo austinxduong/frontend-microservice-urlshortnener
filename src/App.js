baa24155e26fea200c187fe4ec90403ae889a2ea
austinxduong <austinxduong@gmail.com>
2023-09-29 12:20:30 -0700

    fetches data from backend and displays to frontend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+const [data, setData] = useState([]);
+
+const fetchData = () => {
+  return fetch("https://backend-microservice-urlshortener.onrender.com/api/getALL")
+    .then((response) => response.json())
+    .then((data) => setData(data));
+}
+
+useEffect(() => {
+  fetchData();
+  console.log(fetchData())
+}, [])
+
+
   return (
     <div className="App">
       <header className="App-header">
         <h2> Microservice URL shortener</h2>
       </header>
       <body>
-        <button>Fetch Data</button>
+      <button onclick="fetchData()">Fetch Data</button>
+        <ul>
+          {data.map((dataObj, index) => (
+            <li key={dataObj.id}>{dataObj.url}{dataObj.short_url}</li>
+          ))}
+          
+        </ul>
+    
       </body>
     </div>
   );
